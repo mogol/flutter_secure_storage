1ab270b1de3d9de923a0c098e1191b4a2c40c6ae
German Saprykin <german.saprykin@it-nomads.com>
2018-05-19 19:34:44 +0800

    Updated example app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,6 +16,7 @@ class ItemsWidget extends StatefulWidget {
 }
 
 enum _Actions { deleteAll }
+enum _ItemActions { delete, edit }
 
 class _ItemsWidgetState extends State<ItemsWidget> {
   final _storage = new FlutterSecureStorage();
@@ -45,7 +46,7 @@ class _ItemsWidgetState extends State<ItemsWidget> {
 
   void _addNewItem() async {
     final String key = new Uuid().v4();
-    final String value = generateWordPairs().take(5).join(' ');
+    final String value = generateWordPairs().take(2).join(' ');
 
     await _storage.write(key: key, value: value);
     _readAll();
@@ -77,11 +78,70 @@ class _ItemsWidgetState extends State<ItemsWidget> {
         body: new ListView.builder(
           itemCount: _items.length,
           itemBuilder: (BuildContext context, int index) => new ListTile(
+                trailing: new PopupMenuButton(
+                    onSelected: (_ItemActions action) =>
+                        _performAction(action, _items[index]),
+                    itemBuilder: (BuildContext context) =>
+                        <PopupMenuEntry<_ItemActions>>[
+                          new PopupMenuItem(
+                            value: _ItemActions.delete,
+                            child: new Text('Delete'),
+                          ),
+                          new PopupMenuItem(
+                            value: _ItemActions.edit,
+                            child: new Text('Edit'),
+                          ),
+                        ]),
                 title: new Text(_items[index].value),
                 subtitle: new Text(_items[index].key),
               ),
         ),
       );
+
+  Future<Null> _performAction(_ItemActions action, _SecItem item) async {
+    switch (action) {
+      case _ItemActions.delete:
+        await _storage.delete(key: item.key);
+        _readAll();
+
+        break;
+      case _ItemActions.edit:
+        final result = await showDialog<String>(
+            context: context,
+            builder: (context) => new _EditItemWidget(item.value));
+        if (result != null) {
+          _storage.write(key: item.key, value: result);
+          _readAll();
+        }
+        break;
+    }
+  }
+}
+
+class _EditItemWidget extends StatelessWidget {
+  final TextEditingController _controller;
+
+  _EditItemWidget(String text)
+      : _controller = new TextEditingController(text: text);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AlertDialog(
+      title: new Text('Edit item'),
+      content: new TextField(
+        controller: _controller,
+        autofocus: true,
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: new Text('Cancel')),
+        new FlatButton(
+            onPressed: () => Navigator.of(context).pop(_controller.text),
+            child: new Text('Save')),
+      ],
+    );
+  }
 }
 
 class _SecItem {
