a077eb9e7d5f9bdddd61e97d15d00ccdac475bf8
German Saprykin <mogol@users.noreply.github.com>
2020-04-18 11:24:39 +0800

    Update .gitignore.

--- example/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/german/Projects/github/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/german/Projects/nomads/flutter_secure_storage/example"
-export "FLUTTER_TARGET=/Users/german/Projects/nomads/flutter_secure_storage/example/lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/german/Projects/github/flutter/bin/cache/artifacts/engine/ios"
