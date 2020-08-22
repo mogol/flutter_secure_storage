d2943961a664b5f81387e89dfd632ef225c3573e
iassal <assal.ibrahim@gmail.com>
2020-08-22 13:27:50 +0200

    Fix Android crash onDetachedFromEngine when init fails (#145)
    * Fix Android crash onDetachedFromEngine when init fails
    
    * Fix spacing
    
    * Remove extra new line

--- android/src/main/java/com/it_nomads/fluttersecurestorage/FlutterSecureStoragePlugin.java
+++ android/src/main/java/com/it_nomads/fluttersecurestorage/FlutterSecureStoragePlugin.java
@@ -89,8 +89,10 @@ public class FlutterSecureStoragePlugin implements MethodCallHandler, FlutterPlu
 
     @Override
     public void onDetachedFromEngine(FlutterPluginBinding binding) {
-      channel.setMethodCallHandler(null);
-      channel = null;
+      if (channel != null) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+      }
     }
 
     @Override
