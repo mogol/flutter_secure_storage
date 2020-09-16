5d4392f35e6123ae8354685ef1e6d9e34ed45e13
German Saprykin <german.saprykin@it-nomads.com>
2020-09-16 18:35:48 +0300

    Fix pubspec.yaml.

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/germansaprykin/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/germansaprykin/Development/flutter_secure_storage/example"
+export "FLUTTER_TARGET=/Users/germansaprykin/Development/flutter_secure_storage/example/test_driver/app.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/germansaprykin/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
