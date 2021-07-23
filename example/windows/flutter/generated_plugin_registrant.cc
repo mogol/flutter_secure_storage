e38f79a180f58801877c5328e01709942ad42e5e
James Hancock <jhancock@cadlearning.com>
2021-07-23 09:57:31 -0400

    Integrate Windows and MacOs to the library as well based on outstanding PRs.

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+}
---
+++ flutter_secure_storage_windows/README.md
@@ -0,0 +1,3 @@
+# flutter_secure_storage_windows
+
+Windows implementation of Flutter Secure Storage
