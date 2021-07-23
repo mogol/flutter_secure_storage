4c62dddd8e3237e7f41d0f5dc31ebbd1feed6a59
James Hancock <jhancock@cadlearning.com>
2021-07-23 10:06:41 -0400

    Splits out linux per the flutter guideliness. I don't have a computer to test Linux on, but it should be straight forward to fix any issues introduced with the setup of the files etc.

---
+++ flutter_secure_storage_linux/linux/include/Secret.hpp
@@ -0,0 +1,93 @@
+#include "FHashTable.hpp"
+#include <json/json.h>
+#include <libsecret/secret.h>
+#include <memory>
+
+class SecretStorage {
+  FHashTable m_attributes;
+  std::string label;
+  SecretSchema the_schema;
+
+public:
+  const char *getLabel() { return label.c_str(); }
+  void setLabel(const char *label) { this->label = label; }
+
+  SecretStorage(const char *_label = "default") : label(_label) {
+    the_schema = {label.c_str(),
+                  SECRET_SCHEMA_NONE,
+                  {
+                      {"account", SECRET_SCHEMA_ATTRIBUTE_STRING},
+                  }};
+  }
+
+  void addAttribute(const char *key, const char *value) {
+    m_attributes.insert(key, value);
+  }
+
+  bool addItem(const char *key, const char *value) {
+    Json::Value root = readFromKeyring();
+    root[key] = value;
+    return this->storeToKeyring(root);
+  }
+
+  std::string getItem(const char *key) {
+    std::string result;
+    Json::Value root = readFromKeyring();
+    Json::Value resultJson = root[key];
+    if (resultJson.isString()) {
+      result = resultJson.asString();
+      return result;
+    }
+    return "";
+  }
+
+  void deleteItem(const char *key) {
+    Json::Value root = readFromKeyring();
+    root.removeMember(key);
+    this->storeToKeyring(root);
+  }
+
+  bool deleteKeyring() { return this->storeToKeyring(Json::Value()); }
+
+  bool storeToKeyring(Json::Value value) {
+    Json::StreamWriterBuilder builder;
+    const std::string output = Json::writeString(builder, value);
+    std::unique_ptr<GError> err = nullptr;
+    GError *errPtr = err.get();
+
+    builder["indentation"] = "";
+
+    bool result = secret_password_storev_sync(
+        &the_schema, m_attributes.getGHashTable(), nullptr, label.c_str(),
+        output.c_str(), nullptr, &errPtr);
+
+    if (err) {
+      throw err->message;
+    }
+
+    return result;
+  }
+
+  Json::Value readFromKeyring() {
+    Json::Value root;
+    Json::CharReaderBuilder charBuilder;
+    std::unique_ptr<Json::CharReader> reader(charBuilder.newCharReader());
+    std::unique_ptr<GError> err = nullptr;
+    GError *errPtr = err.get();
+
+    const gchar *result = secret_password_lookupv_sync(
+        &the_schema, m_attributes.getGHashTable(), nullptr, &errPtr);
+
+    if (err) {
+      throw err->message;
+    }
+
+    if (result != nullptr && strcmp(result, "") != 0 &&
+        reader->parse(result, result + strlen(result), &root, NULL)) {
+      return root;
+    }
+
+    this->storeToKeyring(root);
+    return root;
+  }
+};
---
+++ flutter_secure_storage_linux/linux/include/flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h
@@ -0,0 +1,27 @@
+#ifndef FLUTTER_PLUGIN_FLUTTER_SECURE_STORAGE_LINUX_PLUGIN_H_
+#define FLUTTER_PLUGIN_FLUTTER_SECURE_STORAGE_LINUX_PLUGIN_H_
+
+#include <flutter_linux/flutter_linux.h>
+
+G_BEGIN_DECLS
+
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __attribute__((visibility("default")))
+#else
+#define FLUTTER_PLUGIN_EXPORT
+#endif
+
+typedef struct _FlutterSecureStorageLinuxPlugin FlutterSecureStorageLinuxPlugin;
+typedef struct
+{
+  GObjectClass parent_class;
+} FlutterSecureStorageLinuxPluginClass;
+
+FLUTTER_PLUGIN_EXPORT GType flutter_secure_storage_plugin_get_type();
+
+FLUTTER_PLUGIN_EXPORT void flutter_secure_storage_plugin_register_with_registrar(
+    FlPluginRegistrar *registrar);
+
+G_END_DECLS
+
+#endif // FLUTTER_PLUGIN_FLUTTER_SECURE_STORAGE_PLUGIN_H_
