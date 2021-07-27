279fa5b14dd14e8c49964a0e8e2a56e3413e9568
James Hancock <jhancock@cadlearning.com>
2021-07-27 08:29:59 -0400

    Remove bogus .gitignores that weren't required and were out of date. Root .gitignore handles everything unless specific platform .gitignore required in sub directory.  This cleans a ton of files that shouldn't have been in the project.
    Updated to use effective_dart.
    Stubberd out unit tests in root project.

---
+++ flutter_secure_storage/test/flutter_secure_storage_unit_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Flutter Secure Storage', () {
+    test('Write should succeed', () {});
+
+    test('Read should succeed', () {});
+
+    test('Read All should succeed', () {});
+
+    test('Contains Key should succeed', () {});
+
+    test('Delete should succeed', () {});
+
+    test('Delete All should succeed', () {});
+  });
+}
