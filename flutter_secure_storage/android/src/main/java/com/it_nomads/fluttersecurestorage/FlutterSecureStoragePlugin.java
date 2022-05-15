cbfa1b733f3a7ecb02bff1a0fd9bfe343f0f40da
German Saprykin <german.saprykin@it-nomads.com>
2022-05-15 12:03:06 +0100

    feat: migrated to EncryptedSharedPreferences

--- flutter_secure_storage/android/src/main/java/com/it_nomads/fluttersecurestorage/FlutterSecureStoragePlugin.java
+++ flutter_secure_storage/android/src/main/java/com/it_nomads/fluttersecurestorage/FlutterSecureStoragePlugin.java
@@ -2,29 +2,16 @@ package com.it_nomads.fluttersecurestorage;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Build;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
-import android.util.Base64;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.security.crypto.EncryptedSharedPreferences;
 import androidx.security.crypto.MasterKey;
 
-import com.it_nomads.fluttersecurestorage.ciphers.StorageCipher;
-import com.it_nomads.fluttersecurestorage.ciphers.StorageCipher18Implementation;
-
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.charset.Charset;
-import java.security.GeneralSecurityException;
-import java.util.HashMap;
 import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -37,104 +24,29 @@ import io.flutter.plugin.common.MethodChannel.Result;
 public class FlutterSecureStoragePlugin implements MethodCallHandler, FlutterPlugin {
 
     private static final String TAG = "FlutterSecureStoragePl";
-    private static final String ELEMENT_PREFERENCES_KEY_PREFIX = "VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIHNlY3VyZSBzdG9yYWdlCg";
     private static final String SHARED_PREFERENCES_NAME = "FlutterSecureStorage";
+
     private MethodChannel channel;
     private SharedPreferences preferences;
-    private SharedPreferences nonEncryptedPreferences;
-    private Charset charset;
-    private StorageCipher storageCipher;
-    // Necessary for deferred initialization of storageCipher.
-    private Context applicationContext;
-    private HandlerThread workerThread;
-    private Handler workerThreadHandler;
-    private boolean useEncryptedSharedPreferences = false;
-    private boolean resetOnError = false;
 
     public void initInstance(BinaryMessenger messenger, Context context) {
         try {
-            applicationContext = context.getApplicationContext();
-            nonEncryptedPreferences = context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
-            charset = Charset.forName("UTF-8");
-
-            workerThread = new HandlerThread("com.it_nomads.fluttersecurestorage.worker");
-            workerThread.start();
-            workerThreadHandler = new Handler(workerThread.getLooper());
+            MasterKey key = new MasterKey.Builder(context)
+                    .setKeyGenParameterSpec(
+                            new KeyGenParameterSpec
+                                    .Builder(MasterKey.DEFAULT_MASTER_KEY_ALIAS, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                                    .setKeySize(256).build())
+                    .build();
+            preferences = EncryptedSharedPreferences.create(context, SHARED_PREFERENCES_NAME, key, EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
 
             channel = new MethodChannel(messenger, "plugins.it_nomads.com/flutter_secure_storage");
             channel.setMethodCallHandler(this);
         } catch (Exception e) {
             Log.e(TAG, "Registration failed", e);
         }
-    }
-
-    private void checkAndMigrateToEncrypted(SharedPreferences source, SharedPreferences target) {
-        for (Map.Entry<String, ?> entry : source.getAll().entrySet()) {
-            Object v = entry.getValue();
-            String key = entry.getKey();
-            if (v instanceof String && key.contains(ELEMENT_PREFERENCES_KEY_PREFIX))
-                try {
-                    final String decodedValue = decodeRawValue((String) v);
-                    target.edit().putString(key, (decodedValue)).apply();
-                    source.edit().remove(key).apply();
-                } catch (Exception e) {
-                    Log.e(TAG, "Data migration failed", e);
-                }
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private void ensureInitialized(Map<String, Object> arguments) {
-        Map<String, Object> options = (Map<String, Object>) arguments.get("options");
-        if (options != null) {
-            useEncryptedSharedPreferences = useEncryptedSharedPreferences(options);
-            resetOnError = resetOnError(options);
-
-            if (storageCipher == null) {
-                try {
-                    storageCipher = new StorageCipher18Implementation(applicationContext);
-                } catch (Exception e) {
-                    Log.e(TAG, "StorageCipher initialization failed", e);
-                }
-            }
-
-            if (useEncryptedSharedPreferences &&
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-                try {
-                    preferences = initializeEncryptedSharedPreferencesManager(applicationContext);
-                } catch (Exception e) {
-                    Log.e("FlutterSecureStoragePl", "EncryptedSharedPreferences initialization failed", e);
-                }
-
-                checkAndMigrateToEncrypted(nonEncryptedPreferences, preferences);
-            } else {
-                preferences = nonEncryptedPreferences;
-            }
-        }
-    }
-
-    private boolean resetOnError(Map<String, Object> arguments) {
-        return arguments.containsKey("resetOnError") && arguments.get("resetOnError").equals("true");
-    }
-
-    private boolean useEncryptedSharedPreferences(Map<String, Object> arguments) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return false;
-        return arguments.containsKey("encryptedSharedPreferences") && arguments.get("encryptedSharedPreferences").equals("true");
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private SharedPreferences initializeEncryptedSharedPreferencesManager(Context context) throws GeneralSecurityException, IOException {
-        MasterKey key = new MasterKey.Builder(context)
-                .setKeyGenParameterSpec(
-                        new KeyGenParameterSpec
-                                .Builder(MasterKey.DEFAULT_MASTER_KEY_ALIAS, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                                .setKeySize(256).build())
-                .build();
-        return EncryptedSharedPreferences.create(context, SHARED_PREFERENCES_NAME, key, EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
     }
 
     @Override
@@ -145,216 +57,74 @@ public class FlutterSecureStoragePlugin implements MethodCallHandler, FlutterPlu
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         if (channel != null) {
-            workerThread.quitSafely();
-            workerThread = null;
-
             channel.setMethodCallHandler(null);
             channel = null;
         }
     }
 
     @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull Result rawResult) {
-        MethodResultWrapper result = new MethodResultWrapper(rawResult);
-        // Run all method calls inside the worker thread instead of the platform thread.
-        workerThreadHandler.post(new MethodRunner(call, result));
-    }
-
     @SuppressWarnings("unchecked")
-    private String getKeyFromCall(MethodCall call) {
-        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-        String rawKey = (String) arguments.get("key");
-        return addPrefixToKey(rawKey);
-    }
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        try {
+            Map<String, Object> arguments = (Map<String, Object>) call.arguments;
+            switch (call.method) {
+                case "write": {
+                    String key =  (String) arguments.get("key");
+                    String value = (String) arguments.get("value");
 
-    @SuppressWarnings("unchecked")
-    private Map<String, String> readAll(boolean useEncryptedSharedPreference) throws Exception {
-        Map<String, String> raw = (Map<String, String>) preferences.getAll();
-
-        Map<String, String> all = new HashMap<>();
-        for (Map.Entry<String, String> entry : raw.entrySet()) {
-            String keyWithPrefix = entry.getKey();
-            if (keyWithPrefix.contains(ELEMENT_PREFERENCES_KEY_PREFIX)) {
-                String key = entry.getKey().replaceFirst(ELEMENT_PREFERENCES_KEY_PREFIX + '_', "");
-                if (useEncryptedSharedPreference) {
-                    all.put(key, entry.getValue());
-                } else {
-                    String rawValue = entry.getValue();
-                    String value = decodeRawValue(rawValue);
-
-                    all.put(key, value);
+                    if (value != null) {
+                        preferences.edit().putString(key,value).apply();
+                        result.success(null);
+                    } else {
+                        result.error("null", null, null);
+                    }
+                    break;
                 }
-            }
-        }
-        return all;
-    }
+                case "read": {
+                    String key = (String) arguments.get("key");
 
-    private void deleteAll() {
-        preferences.edit().clear().apply();
-    }
-
-    private void write(String key, String value, boolean useEncryptedSharedPreference) throws Exception {
-        SharedPreferences.Editor editor = preferences.edit();
-
-        if (useEncryptedSharedPreference) {
-            editor.putString(key, value);
-        } else {
-            byte[] result = storageCipher.encrypt(value.getBytes(charset));
-            editor.putString(key, Base64.encodeToString(result, 0));
-        }
-        editor.apply();
-    }
-
-    private String read(String key, boolean useEncryptedSharedPreference) throws Exception {
-        String rawValue = preferences.getString(key, null);
-        if (useEncryptedSharedPreference) {
-            return rawValue;
-        }
-        return decodeRawValue(rawValue);
-    }
-
-    private void delete(String key) {
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.remove(key);
-        editor.apply();
-    }
-
-    private String addPrefixToKey(String key) {
-        return ELEMENT_PREFERENCES_KEY_PREFIX + "_" + key;
-    }
-
-    private String decodeRawValue(String value) throws Exception {
-        if (value == null) {
-            return null;
-        }
-        byte[] data = Base64.decode(value, 0);
-        byte[] result = storageCipher.decrypt(data);
-
-        return new String(result, charset);
-    }
-
-    /**
-     * MethodChannel.Result wrapper that responds on the platform thread.
-     */
-    static class MethodResultWrapper implements Result {
-
-        private final Result methodResult;
-        private final Handler handler = new Handler(Looper.getMainLooper());
-
-        MethodResultWrapper(Result methodResult) {
-            this.methodResult = methodResult;
-        }
-
-        @Override
-        public void success(final Object result) {
-            handler.post(() -> methodResult.success(result));
-        }
-
-        @Override
-        public void error(final String errorCode, final String errorMessage, final Object errorDetails) {
-            handler.post(() -> methodResult.error(errorCode, errorMessage, errorDetails));
-        }
-
-        @Override
-        public void notImplemented() {
-            handler.post(methodResult::notImplemented);
-        }
-    }
-
-    /**
-     * Wraps the functionality of onMethodCall() in a Runnable for execution in the worker thread.
-     */
-    class MethodRunner implements Runnable {
-        private final MethodCall call;
-        private final Result result;
-
-        MethodRunner(MethodCall call, Result result) {
-            this.call = call;
-            this.result = result;
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public void run() {
-            try {
-                switch (call.method) {
-                    case "write": {
-                        String key = getKeyFromCall(call);
-                        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-                        ensureInitialized(arguments);
-
-                        String value = (String) arguments.get("value");
-
-                        if (value != null) {
-                            write(key, value, useEncryptedSharedPreferences);
-                            result.success(null);
-                        } else {
-                            result.error("null", null, null);
-                        }
-                        break;
-                    }
-                    case "read": {
-                        String key = getKeyFromCall(call);
-                        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-                        ensureInitialized(arguments);
-
-                        if (preferences.contains(key)) {
-                            String value = read(key, useEncryptedSharedPreferences);
-                            result.success(value);
-                        } else {
-                            result.success(null);
-                        }
-                        break;
-                    }
-                    case "readAll": {
-                        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-                        ensureInitialized(arguments);
-
-                        Map<String, String> value = readAll(useEncryptedSharedPreferences);
+                    if (preferences.contains(key)) {
+                        String value = preferences.getString(key, null);
                         result.success(value);
-                        break;
-                    }
-                    case "containsKey": {
-                        String key = getKeyFromCall(call);
-                        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-                        ensureInitialized(arguments);
-
-                        boolean containsKey = preferences.contains(key);
-                        result.success(containsKey);
-                        break;
-                    }
-                    case "delete": {
-                        String key = getKeyFromCall(call);
-                        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-                        ensureInitialized(arguments);
-
-                        delete(key);
+                    } else {
                         result.success(null);
-                        break;
                     }
-                    case "deleteAll": {
-                        Map<String, Object> arguments = (Map<String, Object>) call.arguments;
-                        ensureInitialized(arguments);
-
-                        deleteAll();
-                        result.success(null);
-                        break;
-                    }
-                    default:
-                        result.notImplemented();
-                        break;
+                    break;
                 }
-
-            } catch (Exception e) {
-                if (resetOnError) {
-                    deleteAll();
-                    result.success("Data has been reset");
-                } else {
-                    StringWriter stringWriter = new StringWriter();
-                    e.printStackTrace(new PrintWriter(stringWriter));
-                    result.error("Exception encountered", call.method, stringWriter.toString());
+                case "readAll": {
+                    Map<String, String> value = (Map<String, String>) preferences.getAll();
+                    result.success(value);
+                    break;
                 }
+                case "containsKey": {
+                    String key = (String) arguments.get("key");
+                    boolean containsKey = preferences.contains(key);
+                    result.success(containsKey);
+                    break;
+                }
+                case "delete": {
+                    String key = (String) arguments.get("key");
+
+                    preferences.edit().remove(key).apply();
+                    result.success(null);
+                    break;
+                }
+                case "deleteAll": {
+                    preferences.edit().clear().apply();
+
+                    result.success(null);
+                    break;
+                }
+                default:
+                    result.notImplemented();
+                    break;
             }
+
+        } catch (Exception e) {
+            StringWriter stringWriter = new StringWriter();
+            e.printStackTrace(new PrintWriter(stringWriter));
+            result.error("Exception encountered", call.method, stringWriter.toString());
         }
     }
 }
+
--- flutter_secure_storage/android/src/main/java/com/it_nomads/fluttersecurestorage/ciphers/StorageCipher.java
+++
@@ -1,7 +0,0 @@
-package com.it_nomads.fluttersecurestorage.ciphers;
-
-public interface StorageCipher {
-  byte[] encrypt(byte[] input) throws Exception;
-
-  byte[] decrypt(byte[] input) throws Exception;
-}
