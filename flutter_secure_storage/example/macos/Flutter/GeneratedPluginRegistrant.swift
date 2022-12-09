72bf1be3d22ac386bebe4835b1e1096be0913f1a
Julian Steenbakker <jst@beverinnovations.com>
2022-12-09 10:49:54 +0100

    bug: fixed containsKey on macOS by migrating to swift

---
+++ flutter_secure_storage/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_secure_storage_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftFlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "SwiftFlutterSecureStoragePlugin"))
+}
