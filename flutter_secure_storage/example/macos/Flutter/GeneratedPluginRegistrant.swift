702a32ee451734ce9be777a24d8a46a4c45577cd
yfakariya <yfakariya@gmail.com>
2023-03-20 18:43:57 +0900

    Add/Fix files to run testing app

--- flutter_secure_storage/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_secure_storage/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
